bd04989f0c006cc13349eccf563fdb841dd0001b
Ai Ozaki <aspara1024@gmail.com>
2025-11-14 21:59:40 +0900

    GitHub Actionsでの自動デプロイを設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     port: 3000
-  }
-})
+  },
+  base: mode === 'production' ? '/word-katsuyou/' : '/',
+}))
